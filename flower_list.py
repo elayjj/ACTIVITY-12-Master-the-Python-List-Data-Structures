6db689781dbf22ba74d7daec0ee4561c04f85d33
elijah <layjmadonna@gmail.com>
2024-09-25 15:12:27 +0800

    flower_list.py

---
+++ flower_list.py
@@ -0,0 +1,14 @@
+
+flowers_list= ["Tulip", "Daisy", "Sunflower", "Orchid", "Lily", "Marigold", "Chrysanthemum", "Peony", "Hibiscus", "Lavender"]
+
+print(flowers_list)
+
+print("the 5th index is",flowers_list[4])
+print(flowers_list)
+
+flowers_list[7]= "rose"
+print(flowers_list)
+
+del flowers_list[1]
+
+print("the last index is",flowers_list[-1])
