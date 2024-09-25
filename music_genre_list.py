1d1fc7747134b37f5935b629c77fe0f5346a8ee7
elijah <layjmadonna@gmail.com>
2024-09-26 00:30:49 +0800

    music_genre_list.py

---
+++ music_genre_list.py
@@ -0,0 +1,13 @@
+music_genres = [ "Pop", "Rock", "Hip Hop", "Country", "Classical", "Electronic", "Reggae", "Blues", "R&B", "Metal", "Funk",   "Folk", "Indie", "Latin", "Punk", "Gospel"]
+print(music_genres)
+
+print("the 10th index is",music_genres[9])
+print(music_genres)
+
+music_genres[4]= "Jazz"
+print(music_genres)
+
+del music_genres[6]
+
+print(music_genres[3:8])
+print("the last index is",music_genres[-1])
