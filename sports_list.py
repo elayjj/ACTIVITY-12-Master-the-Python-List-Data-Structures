24b6a2a902945f9a0731d79ab444b1fa6eac5173
elijah <layjmadonna@gmail.com>
2024-09-26 00:14:43 +0800

    sports_list.py

---
+++ sports_list.py
@@ -0,0 +1,12 @@
+sports = ["Soccer", "Billiards", "Tennis", "Cricket", "Baseball", "Rugby", "Golf", "Swimming", "Table Tennis", "Volleyball"]
+print(sports)
+
+print("the 6th index is",sports[5])
+print(sports)
+
+sports[3]= "Basketball"
+print(sports)
+
+del sports[8]
+
+print("the last index is",sports[-1])
