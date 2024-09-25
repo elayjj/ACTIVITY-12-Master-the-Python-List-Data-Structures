6f78f634d5eed76dfb992d1eccd147de5b9f1af5
elijah <layjmadonna@gmail.com>
2024-09-26 00:02:36 +0800

    furniture_list.py

---
+++ furniture_list.py
@@ -0,0 +1,10 @@
+furniture = ["Chair", "Table", "Bed", "Desk", "Wardrobe", "Dresser", "Bookshelf", "Coffee Table", "Nightstand", "Dining Table", "Recliner", "Ottoman"]
+print(furniture)
+
+print("The 8th index is", furniture[7])
+print(furniture)
+
+furniture[4]= "sofa"
+print(furniture)
+del furniture[9]
+print("The last index is",furniture[-1]
