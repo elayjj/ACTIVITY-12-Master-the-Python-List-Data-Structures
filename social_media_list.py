c8dc262432ea839ac92bbd9738d3b61995259535
elijah <layjmadonna@gmail.com>
2024-09-26 00:11:57 +0800

    socialmedia_list.py

---
+++ social_media_list.py
@@ -0,0 +1,12 @@
+social_media = ["Facebook", "Twitter", "LinkedIn", "Snapchat", "TikTok", "Pinterest", "Reddit", "YouTube", "Malban", "Tumblr"]
+print(social_media)
+
+print("the 5th index is",social_media[4])
+print(social_media)
+
+social_media[2]= "Instagram"
+print(social_media)
+
+del social_media[7]
+
+print("the lst index is",social_media[-1])
