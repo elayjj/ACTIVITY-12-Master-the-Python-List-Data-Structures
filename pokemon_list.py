5407d40517c1b3ba534ce0d15aa81b1081b3b1e9
elijah <layjmadonna@gmail.com>
2024-09-25 17:51:25 +0800

    pokemon_list.py

---
+++ pokemon_list.py
@@ -0,0 +1,31 @@
+
+pokemon_list = [
+    "Bulbasaur",
+    "Charmander",
+    "Squirtle",
+    "Pidgey",
+    "Jigglypuff",
+    "Meowth",
+    "Psyduck",
+    "Machop",
+    "Eevee",
+    "Snorlax",
+    "Gengar",
+    "Lapras",
+    "Mewtwo",
+    "Dragonite",
+    "Lucario"
+]
+print(pokemon_list)
+
+print("the 9th index is",pokemon_list[8]
+print(pokemon_list)
+
+pokemon_list[11] = "Pikachu"
+print(pokemon_list)
+
+del pokemon_list[9
+
+print(pokemon_list[4:7])
+
+print("the last index is",pokemon_list[-1])
