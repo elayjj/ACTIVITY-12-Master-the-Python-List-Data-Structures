53466118dd384dd34981839da7b32a77ff889d04
elijah <layjmadonna@gmail.com>
2024-09-25 17:23:21 +0800

    company_list.py

---
+++ company_list.py
@@ -0,0 +1,24 @@
+
+company_list = [
+    "Google",
+    "Microsoft",
+    "Amazon",
+    "Facebook",
+    "Tesla",
+    "Netflix",
+    "IBM",
+    "Samsung",
+    "Intel",
+    "Oracle"
+    ]
+print(company_list)
+
+print("the 7th index is",[6])
+print(company_list)
+
+company_list[4] = "Apple"
+print(company_list)
+
+del company_list[7]
+
+print("the last index is",company_list[-1])
