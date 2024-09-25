8c470402cb3748a9071ce44103d4326dc639bab9
elijah <layjmadonna@gmail.com>
2024-09-25 14:10:25 +0800

    studentlist.py

---
+++ student_list.py
@@ -0,0 +1,16 @@
+student_names = [ "paul","Jinelyn","Jen","kyle","Mat,","christian","Elijah","Alice","Guo","carlos","Andrey","Carol","Kujo","Kuji","cris","danaya","Joy","andrea","nicole","jerome"]
+
+print(student_names)
+
+print("the 15 index is ",student_names[14])
+
+student_names[12] = "john"
+print(student_names)
+
+del student_names[9]
+print(student_names)
+
+print(student_names[2:5])
+
+
+print("the last index is",student_names[-1])
