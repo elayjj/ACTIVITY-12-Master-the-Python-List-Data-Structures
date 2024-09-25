4973cafe258765f1be9a3cdf1a53cd1bdaaa75b8
elijah <layjmadonna@gmail.com>
2024-09-25 15:07:24 +0800

    language_list.py

---
+++ language_list.py
@@ -0,0 +1,16 @@
+
+languages_list = ["Java", "C++", "JavaScript", "C#", "Ruby", "Go", "Swift", "Kotlin", "PHP", "Rust", "TypeScript", "Perl", "R", "Dart"]
+
+print(languages_list)
+
+print("the 11th index is",languages_list[10])
+print(languages_list)
+
+languages_list[8] = "python"
+print(languages_list)
+
+del languages_list[12]
+
+print(languages_list[5:12])
+
+print("the last index is",languages_list[-1])
