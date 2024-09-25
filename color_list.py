fd8c8e2130c130eed889ac18afa3c430db9f81e1
elijah <layjmadonna@gmail.com>
2024-09-25 14:23:22 +0800

    color_list.py

---
+++ color_list.py
@@ -0,0 +1,16 @@
+colors_list = [
+    "Red", "Blue", "Green", "Yellow", "Orange",
+    "Pink", "Brown", "Black", "White",
+    "Gray", "Cyan", "Magenta", "Turquoise"
+]
+print(colors_list)
+
+print("the 10th index is",colors_list[9])
+print(colors_list)
+
+colors_list[3] = "purple"
+print(colors_list)
+
+del colors_list[4]
+
+print("the last index is",colors_list[-1])
