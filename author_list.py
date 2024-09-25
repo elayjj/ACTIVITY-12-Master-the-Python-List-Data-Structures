f6ab3684ce3cce56cae4de9f194a332f31ff3882
elijah <layjmadonna@gmail.com>
2024-09-25 15:15:18 +0800

    author_list.py

---
+++ author_list.py
@@ -0,0 +1,16 @@
+
+author_list = ["William Shakespeare", "Jane Austen", "Charles Dickens", "Ernest Hemingway", "F. Scott Fitzgerald", "Virginia Woolf", "Gabriel García Márquez", "J.K. Rowling", "George Orwell", "Haruki Murakami", "Leo Tolstoy", "Agatha Christie"]
+
+print(author_list)
+
+print("the 6th idex is",author_list[5])
+print(author_list)
+
+author_list[3]= "mark twain"
+print(author_list)
+
+del author_list[9]
+
+print(author_list[3:8])
+
+print("the last index is",author_list[-1])
