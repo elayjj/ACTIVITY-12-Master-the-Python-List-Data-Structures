20eca8ce13672b564fd14b768970c8e7e9bcdfc2
elijah <layjmadonna@gmail.com>
2024-09-25 14:59:30 +0800

    game.list.py

---
+++ game_list.py
@@ -0,0 +1,29 @@
+game_list= [
+    "Roblox",
+    "Generals Command and Conquer",
+    "Red Dead Redemption 2",
+    "Grand Theft Auto V",
+    "Dark Souls",
+    "Fortnite",
+    "Call of Duty: Modern Warfare",
+    "Point Blank",
+    "God of War",
+    "Hollow Knight",
+    "Cyberpunk 2077",
+    "Among Us",
+    "Resident Evil 2",
+    "Super Mario Odyssey",
+    "Apex Legends"
+]
+
+print(game_list)
+
+print("the 7th index is",game_list[6])
+print(game_list)
+
+game_list[8] = "minecraft"
+print(game_list)
+
+print(game_list[5:10])
+
+print("the last index is",game_list[-1])
