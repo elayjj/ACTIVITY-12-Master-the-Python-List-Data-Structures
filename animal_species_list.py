c067d530b4fd69c5fd357fc96a0b24860f45cc48
elijah <layjmadonna@gmail.com>
2024-09-25 20:45:09 +0800

    animal_species_list.py

---
+++ animal_species_list.py
@@ -0,0 +1,11 @@
+animals =  "Lion","Elephant","Tiger","Zebra","Kangaroo","Panda", "Giraffe"
+print (animals)
+print("the 2nd index is",animals[1])
+print(animals)
+
+animals[4]= "Gorilla"
+print(animals)
+
+del animals[5]
+
+print("the last index is",animals[-1])
