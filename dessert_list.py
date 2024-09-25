6bdbc011ebd02e9079b37c15a2012cb0fe4ab9f7
elijah <layjmadonna@gmail.com>
2024-09-25 22:15:02 +0800

    dessert_list.py

---
+++ dessert_list.py
@@ -0,0 +1,12 @@
+desserts = ["Ice Cream", "Brownie", "Cheesecake", "Tiramisu", "Pavlova", "Apple Pie", "Lemon Meringue", "Cupcake", "Panna Cotta", "Macaron"]
+print(desserts)
+
+print("the 4th index is",desserts[3])
+print(desserts)
+
+desserts[5]= "Chocolate Cake"
+print(desserts)
+
+del desserts[2]
+
+print("the last index is",desserts[-1])
