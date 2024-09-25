96e0ef2dbae3306a9ce1db48077490ae89bb6fa4
elijah <layjmadonna@gmail.com>
2024-09-25 17:04:01 +0800

    mountain_list.py

---
+++ mountain_list.py
@@ -0,0 +1,22 @@
+# Create a list of 8 mountain names
+mountain_list= [
+    "Everest",
+    "K2",
+    "Aconcagua",
+    "Fuji",
+    "Aconcagua",
+    "Elbrus",
+    "Annapurna",
+    "Mont Blanc"
+]
+print(mountain_list)
+
+print("the 5th index is",mountain_list[4])
+print(mountain_list)
+
+mountain_list[2] = "Everest"
+print(mountain_list)
+
+del mountain_list[5]
+
+print("the last index is",mountain_list[-1])
