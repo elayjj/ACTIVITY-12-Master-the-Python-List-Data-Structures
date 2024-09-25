6eff9a7facdeb6b773e0e2fd3199a5aa2110edb9
elijah <layjmadonna@gmail.com>
2024-09-25 15:17:40 +0800

    language_spoken_list.py

---
+++ language_spoken_list.py
@@ -0,0 +1,18 @@
+language_list= [
+    "English", "Mandarin", "Hindi", "Arabic", "Urdu",
+    "French", "Bengali", "Russian", "Portuguese", "German",
+    "Japanese", "Korean", "Turkish", "Vietnamese", "Italian",
+    "Thai", "Persian", "Dutch", "Swahili", "Filipino"]
+print(language_list)
+
+print("the 13th inde is",language_list[12])
+print(language_list)
+
+language_list[9] = "spanish"
+print(language_list)
+
+del language_list[15]
+
+print(language_list[6:11])
+
+print("the lst index is",language_list[-1])
