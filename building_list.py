9609df97c8fe87b7c78dde008de0ef53c2f3b706
elijah <layjmadonna@gmail.com>
2024-09-26 00:34:54 +0800

    building_list.py

---
+++ building_list.py
@@ -0,0 +1,12 @@
+buildings = ["Taj Mahal", "Empire State Building", "Burj Khalifa", "Colosseum", "Louvre Museum", "Sydney Opera House", "Petra", "Big Ben"]
+print(buildings)
+
+print("the 5th index is",buildings[4])
+print(buildings)
+
+buildings[1]= "Eifell tower"
+print(buildings)
+
+del buildings[5]
+
+print("the last index is",buildings[-1])
