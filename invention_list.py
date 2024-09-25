f857ee74029c84f4664cccda10b0e7a41b7141b7
elijah <layjmadonna@gmail.com>
2024-09-25 20:56:18 +0800

    invention_list.py

---
+++ invention_list.py
@@ -0,0 +1,11 @@
+invention = ["Telephone", "Light Bulb", "Printing Press", "Airplane", "Internet", "Steam Engine", "Penicillin", "Wheel"]
+print (invention)
+print("the 6th index is"inventions[5])
+print(inventions)
+
+inventions[1] = "Lightbulb"
+print(inventions)
+
+del inventions[4]
+
+print("the last index is",inventions[-1])
