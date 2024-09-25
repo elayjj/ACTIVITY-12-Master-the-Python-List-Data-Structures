6c40c93b412d26bedcb513eed0b6bfd886260ec2
elijah <layjmadonna@gmail.com>
2024-09-25 22:09:46 +0800

    holidays_list.py

--- animal_species_list.py
+++ animal_species_list.py
@@ -1,4 +1,14 @@
-historical_figures = ["George Washington","Cleopatra","Mahatma Gandhi","Albert Einstein","Marie Curie","Winston Churchill","Martin Luther King Jr.","Rosa Parks","Leonardo da Vinci","Marie Antoinette"]
+historical_figures = [holidays = [ "New Year's Day", "Valentine's Day", "Easter", "Thanksgiving", "Independence Day", "Labor Day", "Halloween", "Memorial Day", "Veterans Day", "Hanukkah", "Diwali", "Midsummer"]
+print(holidays)
+
+print("The 9th index is"(holidays[8])
+print (holidays)
+
+holidays[2] = "christmas"
+print(holidays)
+
+del holidays [10]
+print("the last index is" , holidays[-1])
 print(historical_figures)
 
 print("the 8th index is",historical_figures[7])
---
+++ holiday_list.py
@@ -0,0 +1,9 @@
+holidays = [ "New Year's Day","Valentine's Day", "Easter","Thanksgiving", "Independence Day","Labor Day","Halloween","Memorial Day","Veterans Day","Hanukkah","Diwali", "Midsummer"]
+print(holidays)
+print("The 9th index."(holidays[8])
+print (holidays)
+holidays[2] = "christmas"
+print(holidays)
+
+del holidays [11]
+print("the last index is" , holidays[-1])
