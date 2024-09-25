183a815e308319c37d3a8d6598609050e069a949
elijah <layjmadonna@gmail.com>
2024-09-25 15:02:47 +0800

    planet_list.py

---
+++ planet_list.py
@@ -0,0 +1,14 @@
+
+planet_list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+
+print(planet_list)
+
+print("the 3rd index is",planet_list[2])
+print(planet_list)
+
+planet_list[6] = "pluto "
+print(planet_list)
+
+del planet_list[3]
+
+print("the last index is",planet_list[-1])
