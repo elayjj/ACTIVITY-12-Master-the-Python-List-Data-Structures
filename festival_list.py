b59f0e286ff2bb5f60c44a9223722c97bb83243e
elijah <layjmadonna@gmail.com>
2024-09-25 22:17:25 +0800

    dessert_list.py

---
+++ festival_list.py
@@ -0,0 +1,14 @@
+festivals = [ "Carnival", "Oktoberfest", "Holi", "Mardi Gras", "Thanksgiving", "Bastille Day", "Chinese New Year", "Harbin Ice Festival", "La Tomatina", "Gion Matsuri", "Day of the Dead", "Burning Man", "Songkran", "Rio Carnival", "Sapporo Snow Festival"]
+print(festivals)
+
+print("the 7th index is",festivals[6])
+print(festivals)
+
+festivals[10]= "Diwali"
+print(festivals)
+
+del festivals[8]
+
+print(festivals[5:12])
+
+print("the last index is",festivals[-1])
