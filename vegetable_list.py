3b8cae83ddc3b11dbb1f8c108ff8fc3283926a7c
elijah <layjmadonna@gmail.com>
2024-09-25 14:39:53 +0800

    vegetable_list.py

---
+++ vegetable_list.py
@@ -0,0 +1,13 @@
+vegetables_list = ["Carrot", "Broccoli", "Tomato", "Cabbage", "Bell pepper", "Cauliflower"]
+
+print(vegetables_list)
+
+print("the 5th index is",vegetables_list[4])
+print(vegetables_list)
+
+vegetables_list[2] ="spinach"
+print(vegetables_list)
+
+del vegetables_list[5]
+
+print("the last index is",vegetables_list[-1])
