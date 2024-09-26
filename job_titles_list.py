1607d36847b23442c759007d41d999115b60ff16
elijah <layjmadonna@gmail.com>
2024-09-26 14:09:07 +0800

    job_titles_list.py

---
+++ job_titles_list.py
@@ -0,0 +1,12 @@
+job_titles = ["Teacher", "Nurse", "Engineer", "Doctor", "Designer", "Civil engineer", "Marketing Manager", "Data Scientist", "Project Manager", "Sales Representative"]
+print(job_titles)
+
+print("the 6th index is",job_titles[5])
+print(job_titles)
+
+job_titles[4]= "Software Engineer"
+print(job_titles)
+
+del job_titles[8]
+
+print("the lasy index is",job_titles[-1])
