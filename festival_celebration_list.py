d695a362efeb57e211c69ad057829295aebc884c
elijah <layjmadonna@gmail.com>
2024-09-26 15:31:06 +0800

    festival_celebration_list.py

---
+++ festival_celebration_list.py
@@ -0,0 +1,11 @@
+festivals = ["Diwali", "Sinulog Festival", "Ati-Atihan", "Dinagyang", "New Year", "Hanukkah", "Mardi gras", "Lunar New Year", "Oktoberfest", "Bastille Day"]
+print(festivals)
+
+print("the 5th index is",festivals[4])
+print(festivals)
+
+festivals[2]= "Thanksgiving"
+print(festivals)
+
+del festivals[6]
+print("the last index is",festivals[-1])
