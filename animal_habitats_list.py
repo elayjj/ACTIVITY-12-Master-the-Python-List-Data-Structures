ec7139a0b28ce506aea4ae40048e02802bb3e0e3
elijah <layjmadonna@gmail.com>
2024-09-26 13:59:08 +0800

    animal_habitats_list.py

---
+++ animal_habitats_list.py
@@ -0,0 +1,12 @@
+habitats = ["Forest", "Desert", "Rainforest", "Wetland", "Ocean", "Grassland", "Mountain", "Tundra"]
+print(habitats)
+
+print("the 5th index is",habitats[4])
+print(habitats)
+
+habitats[2]= "Savannah"
+print(habitats)
+
+del habitats[5]
+
+print("the last index is",habitats[-1])
