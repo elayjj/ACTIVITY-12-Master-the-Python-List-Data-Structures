f99e867bd8c63178a765625c05022e3cd10c316f
elijah <layjmadonna@gmail.com>
2024-09-26 14:24:06 +0800

    bird_species_list.py

---
+++ bird_species_list.py
@@ -0,0 +1,13 @@
+bird_species = ["Sparrow", "Robin", "Pigeon", "Parrot", "Canary", "Hawk", "Falcon", "Penguin", "Hummingbird" "Woodpecker",  "Blue Jay", "Ostrich"]
+print(bird_species)
+
+print("the 9th index is",bird_species[8])
+print(bird_species)
+
+bird_species[5]= "Eagle"
+print(bird_species)
+
+del bird_species[7]
+
+print(bird_species[3:7])
+print("the last index is",bird_species[-1])
