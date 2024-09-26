afdcdf8396455cc61e55869620b66a4e7a45b027
elijah <layjmadonna@gmail.com>
2024-09-26 13:36:31 +0800

    technology_invention_list.py

---
+++ technology_inventions_list.py
@@ -0,0 +1,14 @@
+inventions = ["Personal Computer", "Internet", "Smart Home Device", "Tablet", "Smartwatch", "3D Printer", "Virtual Reality Headset", "Drone", "Artificial Intelligence", "Blockchain", "Cloud Computing", "Wearable Technology", "Augmented Reality", "Electric Vehicle", "Robotics"]
+print(inventions)
+
+print("the 11th index is",inventions[10])
+print(inventions)
+
+inventions[7]= "Smartphones"
+print(inventions)
+
+del inventions[13]
+
+print(inventions[5:10])
+
+print("the last index is",inventions[-1])
