625cce88a426a8e18756cd93f95d319cffdf4847
elijah <layjmadonna@gmail.com>
2024-09-26 15:13:50 +0800

    recipe_list.py

---
+++ recipe_list.py
@@ -0,0 +1,14 @@
+recipes = "Spaghetti", "Tacos", "Sushi", "Pancakes", "Fried Rice", "Curry", "Grilled Cheese", "Chili", "Moussaka", "Caesar Salad", "Baked Ziti", "Chocolate Cake", "Apple Pie", "Meatloaf", "Beef Stroganoff"
+print(recipes)
+
+print("the 12th index is",recipes[11])
+print(recipes)
+
+recipes[8]= "Lasagna"
+print(recipes)
+
+del recipes[10]
+
+print(recipes[5:10])
+
+print("the last index is",recipes[-1])
