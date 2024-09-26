4dfc8a2bbb2cc91f240230840f3241d09a4bb264
elijah <layjmadonna@gmail.com>
2024-09-26 17:21:31 +0800

    space_missions_list.py

---
+++ space_missions_list.py
@@ -0,0 +1,11 @@
+space = ["Voyager 1", "Hubble Space Telescope", "Mars Rover Curiosity", "International Space Station", "Sputnik 1", "Luna 2", "Cassini-Huygens", "Vostok 1", "Mars Perseverance Rover", "Voyager 1"]
+print(space)
+
+print("The 7th index is",[6])
+print(space)
+
+space(3)= "Apollo 11"
+print(space)
+
+del space[5]
+print("The last index is",space[-1])
