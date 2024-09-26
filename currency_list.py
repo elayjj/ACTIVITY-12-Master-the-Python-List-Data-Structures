d81723c141ff215721ac4f1e4875a72bb0284938
elijah <layjmadonna@gmail.com>
2024-09-26 14:41:46 +0800

    currency_list.py

---
+++ currency_list.py
@@ -0,0 +1,12 @@
+currencies = ["US Dollar", "Japanese Yen", "Thai Baht", "Australian Dollar", "Philippine Peso", "Swiss Franc", "Brazilian Real", "Chinese Yuan", "Indian Rupee", "South Korean Won"]
+print(currencies)
+
+print("the 4th index is",currencies[3])
+print(currencies)
+
+currencies[6]= "Euro"
+print(currencies)
+
+del currencies[8]
+
+print("the last index is",currencies[-1])
