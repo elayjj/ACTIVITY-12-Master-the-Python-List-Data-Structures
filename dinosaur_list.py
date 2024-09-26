9e97168f6771efb388fd7ffa7223a9015cb92bf4
elijah <layjmadonna@gmail.com>
2024-09-26 15:41:16 +0800

    dinosaur_list.py

---
+++ dinosaur_list.py
@@ -0,0 +1,11 @@
+dinosaurs = ["Diplodocus", "Velociraptor", "Triceratops", "Stegosaurus", "Brachiosaurus", "Allosaurus", "Ankylosaurus", "Spinosaurus"  ]
+print(dinosaurs)
+
+print("the 4th index is",dinosaurs[3])
+print(dinosaurs)
+
+dinosaurs[5]= "Tyrannosaurus Rex"
+print(dinosaurs)
+
+del dinosaurs[6]
+print("the last index is",dinosaurs[-1])
