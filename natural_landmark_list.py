2584f921d0acd1a66994851b8cad3eaf1ca5fb73
elijah <layjmadonna@gmail.com>
2024-09-26 15:50:33 +0800

    natural_landmark_list.py

---
+++ natural_landmark_list.py
@@ -0,0 +1,12 @@
+landmarks = ["Mountain Fuji", "Grand Canyon", "Mount Everest", "Great Barrier Reef", "Sahara Desert", "Mayon Volcano", "Chocolate Hills", "Mount Pulag", "El Nido", "Patagonia"]
+print(landmarks)
+
+print("the 8th inde is",landmarks[7])
+print(landmarks)
+
+landmarks[4]= "Grand Canyon"
+print(landmarks)
+
+del landmarks[8]
+
+print("the last index is",landmarks[-1])
