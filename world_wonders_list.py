641d087f5e9b7b8b1940b9d81ec53c0385ffc426
elijah <layjmadonna@gmail.com>
2024-09-26 17:10:15 +0800

    worlds_wonder_list.py

---
+++ world_wonders_list.py
@@ -0,0 +1,12 @@
+wonders = ["Great Pyramid of Giza", "Hanging Gardens of Babylon", "Statue of Zeus at Olympia", "Temple of Artemis at Ephesus", "Mausoleum at Halicarnassus", "Colossus of Rhodes", "Lighthouse of Alexandria"]
+print(wonders)
+
+print("the 4th index is",wonders[3])
+print(wonders)
+
+wonders[1]= "Great Wall of China"
+print(wonders)
+
+del wonders[4]
+
+print("the last index is",wonders[-1])
