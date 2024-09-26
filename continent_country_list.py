7c536bc565e94a6034deda279ec1b9ad8acd049c
elijah <layjmadonna@gmail.com>
2024-09-26 15:37:07 +0800

    continent_country_list.py

---
+++ continent_country_list.py
@@ -0,0 +1,13 @@
+countries = ["Canada", "Brazil", "United Kingdom", "Germany", "India", "China", "Phillipines", "Nigeria", "Russia", "Japan", "South Africa", "Argentina", "Egypt", "Mexico", "Italy" ]
+print(countries)
+
+print("the 9th index is",countries[8])
+print(countries)
+
+countries[9]= "Australia"
+print(countries)
+
+del countries[11]
+
+print(countries[4:8])
+print("the last index is",countries[-1])
